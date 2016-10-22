47107903cb3dc8fa4c01a049ca1e82734fa4a834
Dhron Joshi <dhron.joshi@uwaterloo.ca>
2016-10-21 23:51:07 -0400

    add input valid

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,12 +3,32 @@ import TextField from 'material-ui/TextField';
 
 
 export default class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state={
+      input: "",
+      isValid: true
+    }
+  }
+  validate() {
+    if(this.state.input.length > 5 || this.state.input.length < 0)
+    {
+      this.setState({isValid: false});
+    }
+  }
   render() {
+    //function to check whether inputted text is proper length to search stock
+    this.validate();
     return (
       <TextField className="SearchBar"
-      hintText="Hint Text"
-      floatingLabelText="Fixed Floating Label Text"
-      floatingLabelFixed={true}
+      hintText="Ex. $AAPL"
+      fullWidth={true}
+      floatingLabelText="Enter a stock symbol to generate a card"
+      onTouchTap = {
+          ev => this.setState({input: ev.target.value})
+      }
+      value={this.state.input}
+      floatingLabelFixed={false}
       />
     );
   }
