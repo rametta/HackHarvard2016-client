5ae3cbdbe79bc22c989f87819ca966faa64cd7a0
Dhron Joshi <dhron.joshi@uwaterloo.ca>
2016-10-22 00:44:53 -0400

    modified search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 
 export default class SearchBar extends Component {
@@ -20,16 +22,30 @@ export default class SearchBar extends Component {
     //function to check whether inputted text is proper length to search stock
     this.validate();
     return (
-      <TextField className="SearchBar"
-      hintText="Ex. $AAPL"
-      fullWidth={true}
-      floatingLabelText="Enter a stock symbol to generate a card"
-      onTouchTap = {
-          ev => this.setState({input: ev.target.value})
-      }
-      value={this.state.input}
-      floatingLabelFixed={false}
-      />
+    <div className="SearchBar">
+     <Container fluid >
+
+      <div class="row">
+        <div class="col-md-8">
+          <TextField
+          hintText="Ex. $AAPL"
+          fullWidth={true}
+          floatingLabelText="Enter a stock symbol to generate a card"
+          onChange = {
+              ev => this.setState({input: ev.target.value})
+          }
+          value={this.state.input}
+          floatingLabelFixed={false}
+          />
+       </div>
+       <div class="col-md-4">
+        <FloatingActionButton style="default">
+          <ContentAdd />
+        </FloatingActionButton>
+      </div>
+    </div>
+  />
+</div>
     );
   }
 }
