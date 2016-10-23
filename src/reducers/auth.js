ef416bd3a3710bb5006e0ce1d2df843db804d4b8
Jason <rametta@outlook.com>
2016-10-23 08:02:13 -0400

    auth

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,7 +2,7 @@ import {
   SET_AUTH
 } from '../actions/types';
 
-export default (state = [], action) => {
+export default (state = [{username: 'john', password: 123}], action) => {
   switch (action.type) {
     case SET_AUTH:
       return action.payload;
